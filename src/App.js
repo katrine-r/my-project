5098edc83c25f28368c2c83315c0459b5aae2b22
katrine-r <katrinegantsova@gmail.com>
2021-12-05 17:02:18 +0200

    added component RandomNumber

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import RandomNumber from "./components/RandomNumber/RandomNumber";
 
 function App() {
   return (
     <div className="App">
       <h1>Random Number</h1>
+      <RandomNumber />
     </div>
   );
 }
